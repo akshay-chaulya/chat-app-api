a91aec255f44c7bf9878936b8ff31be614491f43
Akshay Chaulya <akshaychaulya09@gmail.com>
2024-09-29 23:31:43 +0530

    .

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -10,8 +10,9 @@ const generateToken = (res: Response, userId: string) => {
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000, // 15day
         httpOnly: true, // prevent XSS cross site scripting
-        sameSite: "strict", // CSRF attack cross-site request forgery
-        secure: nodeEnv !== "development" // HTTPS
+        sameSite: "none", // CSRF attack cross-site request forgery
+        secure: nodeEnv !== "development", // HTTPS
+
     })
 
     return token;
