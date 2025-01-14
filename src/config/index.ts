020d283679e3813eca2c81fb06a3f14ac8226272
akshay08test <akshaychaulya08@gmail.com>
2025-01-15 00:25:48 +0530

    .

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 
-const port = process.env.PORT || 5000;
-const jwtSecret = process.env.JWT_SECRET!;
-const nodeEnv = process.env.NODE_ENV || "";
+const port: number = parseInt(process.env.PORT || "5000", 10);
+const jwtSecret: string = process.env.JWT_SECRET as string;
+const nodeEnv: string = process.env.NODE_ENV || "";
 
-const origin1 = process.env.ORIGIN1!;
-const origin2 = process.env.ORIGIN2!;
-const origins = [origin1, origin2]
+const origin1: string = process.env.ORIGIN1 as string;
+const origin2: string = process.env.ORIGIN2 as string;
+const origins: string[] = [origin1, origin2];
 
-export { port, jwtSecret, nodeEnv, origins }
+export { port, jwtSecret, nodeEnv, origins };
